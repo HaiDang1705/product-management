1f93ae6a5fdd0119959f83043a21c292f6045291
Dang Nguyen <dangnguyen1705@gmail.com>
2024-06-14 12:12:27 +0700

    danh sach san pham

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,7 +1,22 @@
+const Product = require("../../models/product.model");
+
 // Trang chủ sản phẩm
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: "false"
+    });
+
+    const newProducts = products.map(product => {
+        product.priceNew = (product.price * (100 - product.discountPercentage) / 100).toFixed(0);
+        return product;
+    });
+
+    console.log(newProducts);
+
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Trang sản phẩm"
+        pageTitle: "Trang sản phẩm",
+        products: newProducts
     });
 }
 
